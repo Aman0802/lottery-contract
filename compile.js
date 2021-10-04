04a9bab2823411297f0380f14ce4755c582ff49f
Aman0802 <aman.khubani@gmail.com>
2021-10-04 14:52:51 +0530

    wrote the lottery contract and its tests

--- compile.js
+++ compile.js
@@ -3,8 +3,8 @@ const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
 
-const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
-const source = fs.readFileSync(inboxPath, "utf8");
+const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
+const source = fs.readFileSync(lotteryPath, "utf8");
 
 // the second argument below is used to specify the number of contracts
-module.exports = solc.compile(source, 1).contracts[":Inbox"];
+module.exports = solc.compile(source, 1).contracts[":Lottery"];
--- deploy.js
+++ deploy.js
@@ -16,7 +16,7 @@ const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
   console.log("Attempting to deploy from account", accounts[0]);
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .deploy({ data: bytecode })
     .send({ gas: "1000000", gasPrice: "5000000000", from: accounts[0] });
 
   console.log("Contract deployed at", result.options.address);
